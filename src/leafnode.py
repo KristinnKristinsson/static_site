2d46c4a9a880cf8b376f1d81ebfd1109785c261b
KristinnKristinsson <kristinn.agust.kristinsson@gmail.com>
2025-02-14 14:44:53 +0100

    LeafNode implemented with tests

---
+++ src/leafnode.py
@@ -0,0 +1,19 @@
+from htmlnode import HTMLNode
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+        #self.value = value
+        
+        
+    def to_html(self):
+        
+        if self.value == None:
+            raise ValueError
+        if self.tag == None:
+            return self.value
+        
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __eq__(self, other):
+        return self.tag == other.tag and self.value == other.value and self.props == other.props
---
+++ src/test_leafnode.py
@@ -0,0 +1,25 @@
+import unittest
+
+from leafnode import LeafNode
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_eq(self):
+        node = LeafNode("a","This is a text node", {"href": "https://this.is/a/test"})
+        node2 = LeafNode("p", "This is a text node", None)
+        node3 = LeafNode(None, "A text")
+        result = node.to_html()
+        result2 = node2.to_html()
+        result3 = node3.to_html()
+        self.assertEqual(result, '<a href="https://this.is/a/test">This is a text node</a>')
+        self.assertEqual(result2, '<p>This is a text node</p>')
+        self.assertEqual(result3, "A text")
+    
+    def test_ValueError(self):
+        node4 = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node4.to_html()
+
+
+if __name__ == "__main__":
+    unittest.main()
