85f310da243810a349aa0334f799dec3a56895ee
KristinnKristinsson <kristinn.agust.kristinsson@gmail.com>
2025-05-12 22:13:32 +0200

    boots to the rescue

--- src/static_to_public.py
+++ src/static_to_public.py
@@ -3,6 +3,27 @@ import macromarkdown
 import micromarkdown
 from mdtohtml import markdown_to_html_node
 
+def find_obj_dir():
+    return "./docs"
+
+def find_start_dir():
+    return "../static_site/static"
+
+def find_target_dir(basepath = "/"):
+    try:
+        sys.argv[1]
+        if len(sys.argv) > 1:
+            basepath = sys.argv[1]
+    except IndexError:
+        pass
+    return basepath
+
+def find_template():
+    return "../static_site/template.html"
+
+def find_content():
+    return "../static_site/content"
+
 def delete_public(target):
    if os.path.exists(target):
       for entry in os.listdir(target):
@@ -42,25 +63,9 @@ def navigate_static(start_dir):
 
     return contents
 
-def find_start_dir():
-    return "../static_site/static"
 
-def find_target_dir(basepath = "../static_site/docs"):
-    try:
-        sys.argv[1]
-        if len(sys.argv) > 1:
-            basepath = sys.argv[1]
-    except IndexError:
-        pass
-    return basepath
 
-def find_template():
-    return "../static_site/template.html"
-
-def find_content():
-    return "../static_site/content"
-
-def paste_public_content(source, start, target, template_path):
+def paste_public_content(source, start, target, template_path, basepath):
     os.makedirs(target, exist_ok=True)
 
     for item in source:
@@ -68,29 +73,12 @@ def paste_public_content(source, start, target, template_path):
             for folder_name, nested_contents in item.items():
                 new_source = os.path.join(start, folder_name)
                 new_target = os.path.join(target, folder_name)
-                paste_public_content(nested_contents, new_source, new_target, template_path)
+                paste_public_content(nested_contents, new_source, new_target, template_path, basepath)
         else:
             # It's a file
-            generate_page(start + "/" + item, template_path, target)
+            generate_page(start + "/" + item, template_path, target, basepath)
 
-def create_content():
-    from_path = find_content()
-    template_path = find_template()
-    dest_path = find_target_dir()
-    structure = navigate_static(from_path)
-    paste_public_content(structure, from_path, dest_path, template_path)
-    # paste_public(structure, from_path, dest_path)
-
-    
-
-def static_to_public():
-   start = find_start_dir()
-   target = find_target_dir()
-   source = navigate_static(start)
-#  delete_public(target)
-   paste_public(source, start, target)
-   
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating a webpage from {from_path} to {dest_path} using {template_path}\n Please hold...")
     finalized_article = ""
     md_path = open(from_path)
@@ -100,7 +88,7 @@ def generate_page(from_path, template_path, dest_path):
     html_finalized = markdown_to_html_node(md_content)
     header = micromarkdown.extract_title(md_content)
     rpls_content = re.sub("{{ Content }}", html_finalized, temp_content)
-    rpls_paths = re.sub("{{ basepath }}", dest_path, rpls_content)
+    rpls_paths = re.sub("{{ basepath }}", basepath, rpls_content)
     finalized_article = re.sub("{{ Title }}", header, rpls_paths)
     name_of_file = re.findall(r"\w+.\w+$", from_path)
     the_path = re.sub(r"\w+.\w+", "", from_path)
@@ -109,6 +97,25 @@ def generate_page(from_path, template_path, dest_path):
 
     with open(save_path, "w") as f:
             f.write(finalized_article)
+
+def create_content():
+    from_path = find_content()
+    template_path = find_template()
+    basepath = find_target_dir()
+    dest_path = find_obj_dir()
+    structure = navigate_static(from_path)
+    paste_public_content(structure, from_path, dest_path, template_path, basepath)
+    # paste_public(structure, from_path, dest_path)
+
+def static_to_public():
+   obj_dir = find_obj_dir()
+   start = find_start_dir()
+   target = find_target_dir()
+   source = navigate_static(start)
+   delete_public(obj_dir)
+   paste_public(source, start, obj_dir)
+   
+
 
 
     # header_page = ""
